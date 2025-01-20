5826df5c5790e63ecbae13156926a4d00f2f4160
Hockwood <mthsmercier@gmail.com>
2025-01-20 20:25:52 +0100

    page contact fait

---
+++ public/script/contactScript.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.getElementById('contactForm').addEventListener('submit', async (e) => {
+        e.preventDefault(); // Empêche l'envoi classique du formulaire
+
+        const formData = new FormData(e.target);
+
+        try {
+            const response = await fetch('/contact', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name: formData.get('name'),
+                    email: formData.get('email'),
+                    message: formData.get('message'),
+                }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            if (!response.ok) {
+                throw new Error('Erreur lors de l\'envoi du message.');
+            }
+
+            const result = await response.json();
+
+            // Afficher une alerte avec le message de retour
+            window.alert(`
+                ${result.message}
+                
+                Rappel
+                De : ${result.data.name} (${result.data.email})
+                Message : ${result.data.message}
+                `);
+
+            // Réinitialiser le formulaire
+            e.target.reset();
+        } catch (error) {
+            console.error('Erreur :', error);
+            window.alert('Une erreur est survenue. Veuillez réessayer.');
+        }
+    });
+});
--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -8,6 +8,16 @@ router.get('/', (req, res) => {
     res.render('index', { title: 'Accueil', user: req.user });
 });
 
+router.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact', user: req.user });
+});
 
+router.post('/contact', (req, res) => {
+    const { name, email, message } = req.body;
+
+    console.log(`Message reçu de ${name} (${email}) : ${message}`);
+
+    res.status(200).json({ success: true, message: 'Votre message a été envoyé avec succès !', data : { name, email, message } });
+});
 
 export default router;
