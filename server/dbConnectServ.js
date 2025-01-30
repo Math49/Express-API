d24c477395fad674ea7610d29a1a6c0f817e3fce
Hockwood <mthsmercier@gmail.com>
2025-01-30 08:50:49 +0100

    suppressionn des espaces inutiles

--- server/dbConnectServ.js
+++ server/dbConnectServ.js
@@ -5,9 +5,6 @@ dotenv.config();
 
 Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
     date = this._applyTimezone(date, options);
-
-    // Z here means current timezone, _not_ UTC
-    // return date.format('YYYY-MM-DD HH:mm:ss.SSS Z');
     return date.format('YYYY-MM-DD HH:mm:ss');
 };
 
